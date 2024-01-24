34bea440df8657e3077856b7bdf7f57127316810
Anatoliy Bokachev <a.bokachev@vk.team>
2024-01-24 13:49:55 +0300

    KS-1-5 converted and printed time of Gagarin's space flight

--- src/main/kotlin/lesson1/task5.kt
+++ src/main/kotlin/lesson1/task5.kt
@@ -1,4 +1,11 @@
 package lesson1
 
-class task5 {
+fun main() {
+    val secondsInSpace: Short = 6480
+    val hours = secondsInSpace / 3600
+    val minutes = (secondsInSpace % 3600) / 60
+    val seconds = (secondsInSpace % 3600) % 60
+    println("${calcTime(hours)}:${calcTime(minutes)}:${calcTime(seconds)}")
 }
+
+fun calcTime(timeUnit: Int): String = if (timeUnit < 10) "0$timeUnit" else "$timeUnit"
