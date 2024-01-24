1bf51304c54424e121de3d41e1bbb7aaa912b96b
Anatoliy Bokachev <a.bokachev@vk.team>
2024-01-24 14:30:12 +0300

    KS-4-5 created a function for checking if a vessel is ready for a long voyage

--- src/main/kotlin/lesson4/task5.kt
+++ src/main/kotlin/lesson4/task5.kt
@@ -1,4 +1,17 @@
 package lesson4
 
-class task5 {
+fun main() {
+    println("Пожалуйста, укажите, цел ли корабль (true или false)")
+    val isIntact = readln().toBoolean()
+    println("Пожалуйста, укажите число экипажа (натуральное число)")
+    val crew = readln().toInt()
+    println("Пожалуйста, уажите количество ящиков провизии на борту (натуральное число)")
+    val provision = readln().toInt()
+    println("Пожалуйста, уажите благоприятна ли погода (true или false)")
+    val isWeatherWell = readln().toBoolean()
+
+    println(
+        "Корабль может приступить к долгосрочному плаванию: " +
+                "${(isIntact && crew in 55..70 && provision > 50) || (!isIntact && crew == 70 && isWeatherWell && provision >= 50)}"
+    )
 }
