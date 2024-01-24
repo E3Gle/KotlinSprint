62445b36efa5cba4f0cd29029cb161edcc2b26e4
Anatoliy Bokachev <a.bokachev@vk.team>
2024-01-24 14:20:00 +0300

    KS-4-1 created a function for checking tables availability

--- src/main/kotlin/lesson4/task1.kt
+++ src/main/kotlin/lesson4/task1.kt
@@ -1,4 +1,11 @@
 package lesson4
 
-class task1 {
+fun main() {
+    val totalOfTables = 13
+    val bookedTablesForToday = 13
+    val bookedTablesForTomorrow = 9
+    println(
+        "Доступность столиков на сегодня: ${if (totalOfTables > bookedTablesForToday) "true" else "false"}\n" +
+                "Доступность столиков на завтра: ${if (totalOfTables > bookedTablesForTomorrow) "true" else "false"}"
+    )
 }
