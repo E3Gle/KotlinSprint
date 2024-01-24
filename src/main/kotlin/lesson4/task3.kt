54a2e2b25d09eff4c66f10cbaef1e6d702481af4
Anatoliy Bokachev <a.bokachev@vk.team>
2024-01-24 14:24:02 +0300

    KS-4-3 created a function for checking if conditions are suited for a beans growing

--- src/main/kotlin/lesson4/task3.kt
+++ src/main/kotlin/lesson4/task3.kt
@@ -1,4 +1,14 @@
 package lesson4
 
-class task3 {
+fun main() {
+    val isSunny = true
+    val isTentOpen = true
+    val humidity = 20
+    val season = "Winter"
+
+    areConditionsWell(isSunny, isTentOpen, humidity, season)
+}
+
+fun areConditionsWell(isSunny: Boolean, isTentOpen: Boolean, humidity: Int, season: String) {
+    println("Благоприятные ли условия сейчас для роста бобовых? ${isSunny && isTentOpen && humidity == 20 && season != "Winter"}")
 }
