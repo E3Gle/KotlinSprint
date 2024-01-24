78143d7c1fa9807591e6d11c9ca8aeee8caa8b6a
Anatoliy Bokachev <a.bokachev@vk.team>
2024-01-24 14:27:55 +0300

    KS-4-4 created a function for checking a fitness training schedule

--- src/main/kotlin/lesson4/task4.kt
+++ src/main/kotlin/lesson4/task4.kt
@@ -1,4 +1,22 @@
 package lesson4
 
-class task4 {
+fun main() {
+    val dayOfTraining = 5
+    val armsAndAbsDay: Boolean
+    val legsAndBackDay: Boolean
+
+    if (dayOfTraining % 2 == 0) {
+        legsAndBackDay = true
+        armsAndAbsDay = false
+    } else {
+        legsAndBackDay = false
+        armsAndAbsDay = true
+    }
+
+    println(
+        "Упражнения для рук: $armsAndAbsDay\n" +
+                "Упражнения для ног: $legsAndBackDay\n" +
+                "Упражнения для спины: $armsAndAbsDay\n" +
+                "Упражнения для пресса: $legsAndBackDay"
+    )
 }
