9fc6d2f0ecc75b3f93f258d9ed15d2448627d20f
Anatoliy Bokachev <a.bokachev@vk.team>
2024-01-24 13:59:44 +0300

    KS-2-4 calculated and printed amounts of ore with an active buff

--- src/main/kotlin/lesson2/task4.kt
+++ src/main/kotlin/lesson2/task4.kt
@@ -1,4 +1,14 @@
 package lesson2
 
-class task4 {
+import kotlin.math.roundToInt
+
+fun main() {
+    val buff = 0.2
+    val crystalOre = 7
+    val ironOre = 11
+
+    val crystalOreWithBuff = (crystalOre + (crystalOre * buff)).roundToInt()
+    val ironOreWithBuff = (ironOre + (ironOre * buff)).roundToInt()
+    println("Crystal Ore: $crystalOreWithBuff")
+    println("Iron Ore: $ironOreWithBuff")
 }
