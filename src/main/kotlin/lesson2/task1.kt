edf7e3a7dd4ece6a23971c30610e0ffd3fc30a74
Anatoliy Bokachev <a.bokachev@vk.team>
2024-01-24 13:52:23 +0300

    KS-2-1 calculated and printed average score of students

--- src/main/kotlin/lesson2/task1.kt
+++ src/main/kotlin/lesson2/task1.kt
@@ -1,4 +1,7 @@
 package lesson2
 
-class task1 {
+fun main() {
+    val listOfPupils = listOf(3, 4, 3, 5)
+    val averageScore = listOfPupils.sum() / listOfPupils.size.toDouble()
+    println(String.format("%.2f", averageScore))
 }
