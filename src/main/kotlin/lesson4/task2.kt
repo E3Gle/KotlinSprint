74a696de97d26450ea89b06ef4ccac92f43fabf3
Anatoliy Bokachev <a.bokachev@vk.team>
2024-01-24 14:22:12 +0300

    KS-4-2 created a function for checking if cargo conforms restrictions

--- src/main/kotlin/lesson4/task2.kt
+++ src/main/kotlin/lesson4/task2.kt
@@ -1,4 +1,24 @@
 package lesson4
 
-class task2 {
+fun main() {
+    val pieceOneWeight = 20
+    val pieceOneVolume = 80
+
+    val pieceTwoWeight = 50
+    val pieceTwoVolume = 100
+    checkIfAverage(pieceOneWeight, pieceOneVolume)
+    checkIfAverage(pieceTwoWeight, pieceTwoVolume)
+}
+
+fun checkIfAverage(weight: Int, volume: Int) {
+    val averageWeightMin = 35
+    val averageWeightMax = 100
+    val averageVolume = 100
+    println(
+        "Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': " +
+                "${
+                    (volume <= averageVolume)
+                            && (weight >= averageWeightMin && weight <= averageWeightMax)
+                }"
+    )
 }
