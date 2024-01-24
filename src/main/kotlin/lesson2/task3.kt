a16d1743fb63e11da77cbff64516662c95bd646a
Anatoliy Bokachev <a.bokachev@vk.team>
2024-01-24 13:58:07 +0300

    KS-2-3 calculated and printed train's time of arrival

--- src/main/kotlin/lesson2/task3.kt
+++ src/main/kotlin/lesson2/task3.kt
@@ -1,4 +1,15 @@
 package lesson2
 
-class task3 {
+fun main() {
+    val timeOfDeparture = "9:39"
+    val timeInRoad = 457
+
+    val hoursAndMinutes = timeOfDeparture.split(":")
+    val hoursInMinutes = hoursAndMinutes.first().toInt() * 60
+    val timeOfDepartureInMinutes = hoursInMinutes + hoursAndMinutes.last().toInt()
+
+    val timeOfArrival = timeOfDepartureInMinutes + timeInRoad
+    val timeOfArrivalHours = timeOfArrival / 60
+    val timeOfArrivalMinutes = timeOfArrival % 60
+    println(String.format("%d:%d", timeOfArrivalHours, timeOfArrivalMinutes))
 }
