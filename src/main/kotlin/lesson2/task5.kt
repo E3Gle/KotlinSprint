370a903f044c701a3f1013868df313f177b60df6
Anatoliy Bokachev <a.bokachev@vk.team>
2024-01-24 14:01:13 +0300

    KS-2-5 calculated and printed difficult percent for a deposit

--- src/main/kotlin/lesson2/task5.kt
+++ src/main/kotlin/lesson2/task5.kt
@@ -1,4 +1,15 @@
 package lesson2
 
-class task5 {
+import kotlin.math.pow
+
+fun main() {
+    val deposit = 70_000
+    val interestRate = 16.7
+    val depositTime = 20 //in years
+    val daysInYear = 365 //excluding leap years
+    val interestPaymentInterval = 365 //in days
+    val timesOfInterestPayment = daysInYear / interestPaymentInterval * depositTime
+
+    val depositWithInterest = deposit * (1 + (interestRate / 100) * 365 / 365).pow(timesOfInterestPayment)
+    println(String.format("%.3f", depositWithInterest))
 }
