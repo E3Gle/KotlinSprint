e21153ec3f498c1249f45617fc401c85e13497cd
Anatoliy Bokachev <a.bokachev@vk.team>
2024-01-24 14:03:04 +0300

    KS-3-1 printed greeting message depending on the time of day

--- src/main/kotlin/lesson3/task1.kt
+++ src/main/kotlin/lesson3/task1.kt
@@ -1,4 +1,20 @@
 package lesson3
 
-class task1 {
+fun main() {
+    greeting(true)
+    greeting(false)
+}
+
+fun greeting(isAfternoon: Boolean) {
+    val beforeNoonGreeting = "Good day to you, "
+    val afterNoonGreeting = "Good evening to you, "
+    val userName = "Vasya"
+    val greetingMessage: String
+    if (isAfternoon) {
+        greetingMessage = "$beforeNoonGreeting$userName"
+        println(greetingMessage)
+    } else {
+        greetingMessage = "$afterNoonGreeting$userName"
+        println(greetingMessage)
+    }
 }
