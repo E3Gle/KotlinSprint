2c40d0cfff8edd2e51d6d3a442acb5bdb4dd0123
Anatoliy Bokachev <a.bokachev@vk.team>
2024-01-25 11:10:17 +0300

    KS-5-2 created function that checks if user can view adult content

--- src/main/kotlin/lesson5/task2.kt
+++ src/main/kotlin/lesson5/task2.kt
@@ -1,4 +1,20 @@
 package lesson5
 
-class task2 {
+import java.util.*
+
+fun main() {
+    val currentYear = Calendar.getInstance().weekYear
+
+    println("Введите год вашего рождения")
+    val userYearOfBirth = readln().toInt()
+    val userAge = currentYear - userYearOfBirth
+
+    val result = when {
+        userAge >= AGE_OF_MAJORITY -> "Показать экран со скрытым контентом"
+        else -> "Вы должны быть старше 18 лет для просмотра данного контента"
+    }
+
+    println(result)
 }
+
+const val AGE_OF_MAJORITY = 18
