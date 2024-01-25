764d22e2ce4a6f85a7141fa49ed256dea0438ad0
Anatoliy Bokachev <a.bokachev@vk.team>
2024-01-25 16:02:16 +0300

    KS-3-1 changed the logic of the greeting message output

--- src/main/kotlin/lesson3/task1.kt
+++ src/main/kotlin/lesson3/task1.kt
@@ -1,20 +1,13 @@
 package lesson3
 
 fun main() {
-    greeting(true)
-    greeting(false)
-}
-
-fun greeting(isAfternoon: Boolean) {
     val beforeNoonGreeting = "Good day to you, "
     val afterNoonGreeting = "Good evening to you, "
     val userName = "Vasya"
-    val greetingMessage: String
-    if (isAfternoon) {
-        greetingMessage = "$beforeNoonGreeting$userName"
-        println(greetingMessage)
-    } else {
-        greetingMessage = "$afterNoonGreeting$userName"
-        println(greetingMessage)
-    }
+
+    var greetingMessage = "$beforeNoonGreeting$userName"
+    println(greetingMessage)
+
+    greetingMessage = "$afterNoonGreeting$userName"
+    println(greetingMessage)
 }
