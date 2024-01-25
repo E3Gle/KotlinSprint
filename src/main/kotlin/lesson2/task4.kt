7fbe5e4995d06a69439389a05c1a7604b625a4fa
Anatoliy Bokachev <a.bokachev@vk.team>
2024-01-25 15:25:31 +0300

    KS-2-4 changed the logic of the end result calculations

--- src/main/kotlin/lesson2/task4.kt
+++ src/main/kotlin/lesson2/task4.kt
@@ -3,12 +3,13 @@ package lesson2
 import kotlin.math.roundToInt
 
 fun main() {
-    val buff = 0.2
+    val buff = 20
     val crystalOre = 7
     val ironOre = 11
 
-    val crystalOreWithBuff = (crystalOre + (crystalOre * buff)).roundToInt()
-    val ironOreWithBuff = (ironOre + (ironOre * buff)).roundToInt()
-    println("Crystal Ore: $crystalOreWithBuff")
-    println("Iron Ore: $ironOreWithBuff")
+    val crystalOreWithBuff = (crystalOre + (crystalOre * (buff / 100.0)))
+    val ironOreWithBuff = (ironOre + (ironOre * (buff / 100.0)))
+
+    println("Bonus Crystal Ore: ${(crystalOreWithBuff - crystalOre).roundToInt()}")
+    println("Bonus Iron Ore: ${(ironOreWithBuff - ironOre).roundToInt()}")
 }
