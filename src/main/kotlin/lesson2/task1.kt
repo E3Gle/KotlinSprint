442945ae7d697cf1ac4ed5779898d6792ac3befc
Anatoliy Bokachev <a.bokachev@vk.team>
2024-01-25 09:10:44 +0300

    KS-2-1 changed approach to storing students data

--- src/main/kotlin/lesson2/task1.kt
+++ src/main/kotlin/lesson2/task1.kt
@@ -1,7 +1,7 @@
 package lesson2
 
 fun main() {
-    val listOfPupils = listOf(3, 4, 3, 5)
-    val averageScore = listOfPupils.sum() / listOfPupils.size.toDouble()
+    val studentsAndScores = mapOf("Chris" to 3, "Leon" to 4, "Jill" to 3, "Claire" to 5)
+    val averageScore = studentsAndScores.values.sum() / studentsAndScores.values.size.toDouble()
     println(String.format("%.2f", averageScore))
 }
