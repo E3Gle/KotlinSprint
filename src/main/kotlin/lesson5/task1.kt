13b6d712fb4a1f706dc55b2db61f6b12a33d935b
Anatoliy Bokachev <a.bokachev@vk.team>
2024-01-25 10:46:12 +0300

    KS-5-1 created simple authentication factor

--- src/main/kotlin/lesson5/task1.kt
+++ src/main/kotlin/lesson5/task1.kt
@@ -1,4 +1,16 @@
 package lesson5
 
-class task1 {
+fun main() {
+    println("Для входа в приложение необходимо доказать, что вы - не робот.\nВам предстоит написать и решить простой арифметический пример")
+    println("Введите первое произвольное натуральное число:")
+    val firstNumber = readln().toInt()
+    println("Введите второе произвольное натуральное число:")
+    val secondNumber = readln().toInt()
+    println("Введите сумму раннее введённых чисел:")
+    val sumOfNumbers = readln().toInt()
+    if (sumOfNumbers == (firstNumber + secondNumber)) {
+        println("Добро пожаловать!")
+    } else {
+        println("Доступ запрещен.")
+    }
 }
