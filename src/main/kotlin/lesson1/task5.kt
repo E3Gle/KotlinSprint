b31335caa368fd52e9c018e30410a1ba8abfd960
Anatoliy Bokachev <a.bokachev@vk.team>
2024-01-25 09:06:14 +0300

    KS-1-5 replaced magic numbers with constants

--- src/main/kotlin/lesson1/task5.kt
+++ src/main/kotlin/lesson1/task5.kt
@@ -2,10 +2,12 @@ package lesson1
 
 fun main() {
     val secondsInSpace: Short = 6480
-    val hours = secondsInSpace / 3600
-    val minutes = (secondsInSpace % 3600) / 60
-    val seconds = (secondsInSpace % 3600) % 60
+    val hours = secondsInSpace / SECONDS_IN_HOUR
+    val minutes = (secondsInSpace % SECONDS_IN_HOUR) / SECONDS_IN_MINUTE
+    val seconds = (secondsInSpace % SECONDS_IN_HOUR) % SECONDS_IN_MINUTE
     println("${calcTime(hours)}:${calcTime(minutes)}:${calcTime(seconds)}")
 }
 
 fun calcTime(timeUnit: Int): String = if (timeUnit < 10) "0$timeUnit" else "$timeUnit"
+const val SECONDS_IN_MINUTE = 60
+const val SECONDS_IN_HOUR = 3600
