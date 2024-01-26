bda9974632f06979e7465a95220be9870b561f1a
Anatoliy Bokachev <a.bokachev@vk.team>
2024-01-26 11:04:14 +0300

    KS-6-4 created a guessing game

--- src/main/kotlin/lesson6/task4.kt
+++ src/main/kotlin/lesson6/task4.kt
@@ -1,4 +1,23 @@
 package lesson6
 
-class task4 {
+import kotlin.random.Random
+
+fun main() {
+    var attemptsCounter = 5
+    val targetNumber = Random.nextInt(10)
+
+    println("Вам предстоит угадать число от 0 до 9. У вас есть 5 попыток.")
+
+    while (attemptsCounter > 0) {
+        println("Введите число: ")
+        when (readln().toInt()) {
+            targetNumber -> {
+                println("Это была великолепная игра!")
+                break
+            }
+            else -> println("Неверно. Осталось попыток: ${--attemptsCounter}")
+        }
+    }
+
+    if (attemptsCounter == 0) println("Было загадано число: $targetNumber")
 }
