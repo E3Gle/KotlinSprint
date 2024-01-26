dbe462983cae8b61420722a59d7062b6fb80d7c4
Anatoliy Bokachev <a.bokachev@vk.team>
2024-01-26 11:31:29 +0300

    KS-6-5 created advanced authentication flow

--- src/main/kotlin/lesson6/task5.kt
+++ src/main/kotlin/lesson6/task5.kt
@@ -1,4 +1,41 @@
 package lesson6
 
-class task5 {
+import kotlin.random.Random
+
+fun main() {
+    println("Для входа в систему необходимо создать учётную запись. Для этого следуйте инструкциям на экране.\nУкажите желаемый логин: ")
+    val userLogin = readln()
+    println("Укажите желаемый пароль:")
+    val userPassword = readln()
+    println("Поздравляем, учётная запись создана!")
+
+    println("Для входа в систему необходимо авторизоваться.\nВведите логин вашей учётной записи: ")
+    val inputtedLogin = readln()
+    println("Введите пароль вашей учётной записи: ")
+    val inputtedPassword = readln()
+
+    if (inputtedLogin == userLogin && inputtedPassword == userPassword) {
+        var firstNumber = Random.nextInt(1, 10)
+        var secondNumber = Random.nextInt(1, 10)
+        var attempts = 3
+
+        println("Для входа необходимо подтвердить, что вы не робот.\nВведите результат сложения двух чисел: $firstNumber и $secondNumber.\nУ вас три попытки")
+        while (attempts > 0) {
+            when (readln().toInt()) {
+                firstNumber + secondNumber -> {
+                    println("Добро пожаловать!")
+                    break
+                }
+
+                else -> {
+                    firstNumber = Random.nextInt(1, 10)
+                    secondNumber = Random.nextInt(1, 10)
+                    println("Неверно. Введите результат сложения двух чисел: $firstNumber и $secondNumber. У вас осталось ${--attempts}")
+                }
+            }
+        }
+
+    } else {
+        println("Данные введены неверно. Введите заново ваши логин и пароль.")
+    }
 }
