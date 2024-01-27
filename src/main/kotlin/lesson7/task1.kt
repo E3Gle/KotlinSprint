5dbff516776edd1e4981c8615ca8286e6bf5e734
Anatoliy Bokachev <a.bokachev@vk.team>
2024-01-27 12:24:25 +0300

    KS-7-1 created a password generating function

--- src/main/kotlin/lesson7/task1.kt
+++ src/main/kotlin/lesson7/task1.kt
@@ -1,4 +1,15 @@
 package lesson7
 
-class task1 {
+fun main() {
+    val letters: CharRange = 'a'..'z'
+    val digits: IntRange = 0..9
+    var counter = 0
+    var result = ""
+
+    while (counter < 6) {
+        if (counter % 2 == 0) result += letters.random()
+        else result += digits.random()
+        counter++
+    }
+    println(result)
 }
