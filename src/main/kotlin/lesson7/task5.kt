d8f4dc76dab910e008e88d74ede8bcbb71a56eb6
Anatoliy Bokachev <a.bokachev@vk.team>
2024-01-27 13:53:32 +0300

    KS-7-5 created more advanced password generating function

--- src/main/kotlin/lesson7/task5.kt
+++ src/main/kotlin/lesson7/task5.kt
@@ -1,4 +1,25 @@
 package lesson7
 
-class task5 {
+import kotlin.random.Random
+
+fun main() {
+    val letters: CharRange = 'a'..'z'
+    val capitalLetters = 'A'..'Z'
+    val digits: IntRange = 0..9
+    var password = ""
+
+    println("Укажите максимальную длину желаемого пароля либо нажмите Enter, чтобы получить пароль с длиной по умолчанию: ")
+    val userInput = readln()
+    val passwordLength = if (userInput == "") MIN_PASSWORD_LENGTH else userInput.toInt()
+
+    while (password.length < passwordLength) {
+        when (Random.nextInt(3)) {
+            0 -> password += letters.random()
+            1 -> password += capitalLetters.random()
+            else -> password += digits.random()
+        }
+    }
+    println(password)
 }
+
+const val MIN_PASSWORD_LENGTH = 6
