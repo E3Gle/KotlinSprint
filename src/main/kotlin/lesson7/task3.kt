f41d1a03067c88030a3091cdd4af622412f65f5f
Anatoliy Bokachev <a.bokachev@vk.team>
2024-01-27 12:56:57 +0300

    KS-7-3 created an even numbers counting function

--- src/main/kotlin/lesson7/task3.kt
+++ src/main/kotlin/lesson7/task3.kt
@@ -1,4 +1,15 @@
 package lesson7
 
-class task3 {
+fun main() {
+    println("Укажите число: ")
+
+    val result = mutableListOf<String>()
+    val userInput = readln().toInt()
+
+    val evenNumbersProgression = 0..userInput step 2
+
+    for (i in evenNumbersProgression) {
+        result.add("$i")
+    }
+    println(result.joinToString(", "))
 }
