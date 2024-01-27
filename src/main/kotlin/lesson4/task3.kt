4ea335fdbd57a12aed7fee16d472d67f553fd7b1
Anatoliy Bokachev <a.bokachev@vk.team>
2024-01-27 11:52:27 +0300

    KS-4-3 moved comparable values into variables

--- src/main/kotlin/lesson4/task3.kt
+++ src/main/kotlin/lesson4/task3.kt
@@ -1,13 +1,23 @@
 package lesson4
 
 fun main() {
+    val isSunnyToday = true
+    val isTentOpenNow = true
+    val humidityToday = 20
+    val seasonNow = "Winter"
+
     println(
         "Благоприятные ли условия сейчас для роста бобовых? " +
-                "${IS_SUNNY && IS_TENT_OPEN && HUMIDITY == 20 && SEASON != "Winter"}"
+                "${
+                    (IS_SUNNY == isSunnyToday)
+                            && (IS_TENT_OPEN == isTentOpenNow)
+                            && (HUMIDITY == humidityToday)
+                            && (WRONG_SEASON != seasonNow)
+                }"
     )
 }
 
 const val IS_SUNNY = true
 const val IS_TENT_OPEN = true
 const val HUMIDITY = 20
-const val SEASON = "Winter"
+const val WRONG_SEASON = "Winter"
