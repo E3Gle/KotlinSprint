6d5a6af8730290f84d8e0e82386cf8aa6c0d259f
Anatoliy Bokachev <a.bokachev@vk.team>
2024-01-27 13:04:34 +0300

    KS-7-4 created a timer using range and for-loop

--- src/main/kotlin/lesson7/task4.kt
+++ src/main/kotlin/lesson7/task4.kt
@@ -1,4 +1,15 @@
 package lesson7
 
-class task4 {
+fun main() {
+    println("Укажите сколько секунд вы хотите засечь: ")
+    val userInput = readln().toInt()
+    val secondsToCount = 1..userInput
+
+    for (s in secondsToCount.reversed()) {
+        println("Секунд осталось: $s")
+        Thread.sleep(MILLISECONDS_IN_SECOND)
+    }
+    println("Время вышло")
 }
+
+const val MILLISECONDS_IN_SECOND = 1000L
