74c858184f8acc9790311e29137652feb0f9a796
Anatoliy Bokachev <a.bokachev@vk.team>
2024-01-28 19:48:18 +0300

    KS-7-1 changed password generation mechanism from while-loop to for-loop

--- src/main/kotlin/lesson7/task1.kt
+++ src/main/kotlin/lesson7/task1.kt
@@ -3,13 +3,14 @@ package lesson7
 fun main() {
     val letters: CharRange = 'a'..'z'
     val digits: IntRange = 0..9
-    var counter = 0
+    val passwordRange = 1..PASSWORD_SIZE
     var result = ""
 
-    while (counter < 6) {
-        if (counter % 2 == 0) result += letters.random()
-        else result += digits.random()
-        counter++
+    for (i in passwordRange) {
+        if (i % 2 == 0) result += digits.random()
+        else result += letters.random()
     }
     println(result)
 }
+
+const val PASSWORD_SIZE = 6
