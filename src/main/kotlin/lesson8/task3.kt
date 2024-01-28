48b8fb94ddb81bcee906ae3c62bce36a2dc53db3
Anatoliy Bokachev <a.bokachev@vk.team>
2024-01-28 10:41:47 +0300

    KS-8-3 created advanced ingredient checking function

--- src/main/kotlin/lesson8/task3.kt
+++ src/main/kotlin/lesson8/task3.kt
@@ -1,4 +1,11 @@
 package lesson8
 
-class task3 {
+fun main() {
+    val ingredients = arrayOf("Сосиски", "Мука", "Яблоки", "Ром", "Кокос")
+    println("Введите название ингредиента, который вы хотите найти: ")
+    val userInput = readln()
+
+    if (ingredients.find { it == userInput }?.isNotEmpty() == true)
+        println("Ингредиент $userInput в рецепте есть")
+    else println("Такого ингредиента в рецепте нет")
 }
