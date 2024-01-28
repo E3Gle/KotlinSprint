b47d92727fe6d26b23ef63c469914915ae1291b7
Anatoliy Bokachev <a.bokachev@vk.team>
2024-01-28 10:34:08 +0300

    KS-8-2 created ingredient checking function

--- src/main/kotlin/lesson8/task2.kt
+++ src/main/kotlin/lesson8/task2.kt
@@ -1,4 +1,14 @@
 package lesson8
 
-class task2 {
+fun main() {
+    val ingredients = arrayOf("Сосиски", "Мука", "Яблоки", "Ром", "Кокос")
+    println("Введите название ингредиента, который вы хотите найти: ")
+    val userInput = readln()
+    var isIngredientIn = false
+
+    for (i in ingredients) {
+        if (i == userInput) isIngredientIn = true
+    }
+
+    if (isIngredientIn) println("Ингредиент $userInput в рецепте есть") else println("Такого ингредиента в рецепте нет")
 }
