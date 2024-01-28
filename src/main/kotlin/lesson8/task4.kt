c9b4f221fc8e997f1d877d3ce86396888d482f25
Anatoliy Bokachev <a.bokachev@vk.team>
2024-01-28 10:52:43 +0300

    KS-8-4 created ingredient replacing function

--- src/main/kotlin/lesson8/task4.kt
+++ src/main/kotlin/lesson8/task4.kt
@@ -1,4 +1,20 @@
 package lesson8
 
-class task4 {
+fun main() {
+    val ingredients = arrayListOf("Сосиски", "Мука", "Яблоки", "Ром", "Кокос")
+
+    println(ingredients)
+    println("Укажите, какой ингредиент вы хотите заменить: ")
+    val inputtedIngredient = readln()
+
+    if (ingredients.find { it == inputtedIngredient }.isNullOrEmpty()) {
+        println("Такого ингредиента в списке нет")
+        return
+    } else println("На какой ингредиент вы бы хотели его заменить: ")
+    val inputtedIngredientReplacement = readln()
+
+    val originalIngredientIndex = ingredients.indexOf(inputtedIngredient)
+    ingredients.remove(inputtedIngredient)
+    ingredients.add(originalIngredientIndex, inputtedIngredientReplacement)
+    println("Готово! Вы сохранили следующий список: $ingredients")
 }
