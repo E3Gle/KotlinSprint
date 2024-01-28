d3315408b65f1aa27f736a496a19aa6dc3f68bb0
Anatoliy Bokachev <a.bokachev@vk.team>
2024-01-28 11:26:19 +0300

    KS-8-5 created a function that creates an array with a size specified by the user

--- src/main/kotlin/lesson8/task5.kt
+++ src/main/kotlin/lesson8/task5.kt
@@ -1,4 +1,16 @@
 package lesson8
 
-class task5 {
+fun main() {
+    println("Введите количество ингредиентов в рецепте: ")
+    val ingredientsCount = readln().toInt()
+    var counter = 0
+    var ingredients = arrayOf<String>()
+
+    while (counter < ingredientsCount) {
+        println("Введите название ингредиента для добавления в список: ")
+        val userInput = readln()
+        ingredients = ingredients.plus(userInput)
+        counter++
+        if (counter == ingredientsCount) println(ingredients.joinToString(", "))
+    }
 }
