ab3299225c2d55c23e6cf0e6ac2ab2228872c06d
Anatoliy Bokachev <a.bokachev@vk.team>
2024-01-28 20:00:09 +0300

    KS-6-4 got rid of redundant library import

--- src/main/kotlin/lesson6/task4.kt
+++ src/main/kotlin/lesson6/task4.kt
@@ -1,23 +1,22 @@
 package lesson6
 
-import kotlin.random.Random
-
 fun main() {
     var attemptsCounter = 5
-    val targetNumber = Random.nextInt(10)
+    val targetNumber = (0..9).random()
 
     println("Вам предстоит угадать число от 0 до 9. У вас есть 5 попыток.")
 
-    while (attemptsCounter > 0) {
+    do {
         println("Введите число: ")
         when (readln().toInt()) {
             targetNumber -> {
                 println("Это была великолепная игра!")
                 break
             }
+
             else -> println("Неверно. Осталось попыток: ${--attemptsCounter}")
         }
-    }
+    } while (attemptsCounter > 0)
 
     if (attemptsCounter == 0) println("Было загадано число: $targetNumber")
 }
