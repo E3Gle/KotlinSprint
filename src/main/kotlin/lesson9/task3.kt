43adcf2c30b596fd1fd7188dbd445914bdeb3ee5
Anatoliy Bokachev <a.bokachev@vk.team>
2024-01-29 10:22:34 +0300

    KS-9-3 created a function for a portion ingredients amount calculation

--- src/main/kotlin/lesson9/task3.kt
+++ src/main/kotlin/lesson9/task3.kt
@@ -1,4 +1,12 @@
 package lesson9
 
-class task3 {
+fun main() {
+    val omeletteIngredients = mutableListOf(2, 50, 15)
+
+    println("Как много порций вы бы хотели приготовить?")
+    val userInput = readln().toInt()
+    println(
+        "На $userInput порций вам понадобится: Яиц – ${omeletteIngredients[0] * userInput}, " +
+                "молока – ${omeletteIngredients[1] * userInput}, сливочного масла - ${omeletteIngredients[2] * userInput}"
+    )
 }
