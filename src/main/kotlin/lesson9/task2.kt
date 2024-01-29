baded9c379f144a740507870411404acb6c20487
Anatoliy Bokachev <a.bokachev@vk.team>
2024-01-29 10:14:25 +0300

    KS-9-2 created a function for adding user-inputted ingredients to a recipe, also got rid of multiple constant declaration in another task

--- src/main/kotlin/lesson9/task2.kt
+++ src/main/kotlin/lesson9/task2.kt
@@ -1,4 +1,19 @@
 package lesson9
 
-class task2 {
+fun main() {
+    val ingredients = mutableListOf("Мука", "Вода", "Масло")
+
+    println("В рецепте есть базовые ингредиенты: ")
+    ingredients.forEach { println(it) }
+
+    println("Желаете добавить еще?")
+    var userInput = readln()
+    if (userInput.lowercase() == "нет") return
+    else {
+        println("Какой ингредиент вы хотите добавить?")
+        userInput = readln()
+        ingredients.add(userInput)
+        println("Теперь в рецепте есть следующие ингредиенты: ")
+        ingredients.forEach { println(it) }
+    }
 }
