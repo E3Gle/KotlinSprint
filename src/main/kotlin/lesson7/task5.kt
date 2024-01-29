5900148ea1403d70780284a8b2bf5b5a169f28cd
Anatoliy Bokachev <a.bokachev@vk.team>
2024-01-29 09:08:09 +0300

    KS-7-5 got rid of redundant library import

--- src/main/kotlin/lesson7/task5.kt
+++ src/main/kotlin/lesson7/task5.kt
@@ -1,11 +1,10 @@
 package lesson7
 
-import kotlin.random.Random
-
 fun main() {
     val letters: CharRange = 'a'..'z'
     val capitalLetters = 'A'..'Z'
     val digits: IntRange = 0..9
+    val randomizerCounter = 0..2
     var password = ""
 
     println("Укажите максимальную длину желаемого пароля либо нажмите Enter, чтобы получить пароль с длиной по умолчанию: ")
@@ -13,7 +12,7 @@ fun main() {
     val passwordLength = if (userInput == "") MIN_PASSWORD_LENGTH else userInput.toInt()
 
     while (password.length < passwordLength) {
-        when (Random.nextInt(3)) {
+        when (randomizerCounter.random()) {
             0 -> password += letters.random()
             1 -> password += capitalLetters.random()
             else -> password += digits.random()
