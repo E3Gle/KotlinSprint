720b9ad79fcc5a75c2adb7e2d0aecb0b3a8fa93a
Anatoliy Bokachev <a.bokachev@vk.team>
2024-01-30 08:43:16 +0300

    KS-8-1 changed reduce function to a sumOf function

--- src/main/kotlin/lesson8/task1.kt
+++ src/main/kotlin/lesson8/task1.kt
@@ -2,5 +2,5 @@ package lesson8
 
 fun main() {
     val weeklyViews = arrayOf(12, 42, 565, 22, 78, 973, 102)
-    println(weeklyViews.reduce { acc, i -> acc + i })
+    println(weeklyViews.sumOf { it })
 }
