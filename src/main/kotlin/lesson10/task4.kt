a5132703c6e17cbf35a6fab8ee651cbe032e0efc
Anatoliy Bokachev <a.bokachev@vk.team>
2024-01-30 10:52:24 +0300

    KS-10-4 created an advanced dice throwing game

--- src/main/kotlin/lesson10/task4.kt
+++ src/main/kotlin/lesson10/task4.kt
@@ -1,4 +1,37 @@
 package lesson10
 
-class task4 {
+fun main() {
+
+    var userVictories = 0
+    var userChoice: String
+
+    do {
+        userVictories += playGame()
+        println("Хотите бросить кости еще раз? Введите Да или Нет")
+        userChoice = readln()
+    } while (userChoice.lowercase() != "нет")
+
+    println("Всего вы выиграли $userVictories партий")
+}
+
+fun diceThrow(): Int = (1..6).random()
+
+fun playGame(): Int {
+    val userResult = diceThrow()
+    println("Вы бросаете кубик, и выпадает: $userResult")
+
+    val aiResult = diceThrow()
+    println("Компьютер бросает кубик, и выпадает: $aiResult")
+
+    val isPlayerWon: Boolean
+
+    val result = when {
+        userResult < aiResult -> "Победили машины"
+        userResult > aiResult -> "Победило человество"
+        else -> "Ничья"
+    }
+    println(result)
+    isPlayerWon = result == "Победило человество"
+
+    return if (isPlayerWon) 1 else 0
 }
