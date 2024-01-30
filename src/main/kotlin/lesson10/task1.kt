4b04670353b502d231605ea44e8e5df4132d33ab
Anatoliy Bokachev <a.bokachev@vk.team>
2024-01-30 10:14:46 +0300

    KS-10-1 created a simple dice throwing game

--- src/main/kotlin/lesson10/task1.kt
+++ src/main/kotlin/lesson10/task1.kt
@@ -1,4 +1,18 @@
 package lesson10
 
-class task1 {
+fun main() {
+    val userResult = diceThrow()
+    println("Вы бросаете кубик, и выпадает: $userResult")
+
+    val aiResult = diceThrow()
+    println("Компьютер бросает кубик, и выпадает: $aiResult")
+
+    val result = when {
+        userResult < aiResult -> "Победили машины"
+        userResult > aiResult -> "Победило человество"
+        else -> "Ничья"
+    }
+    println(result)
 }
+
+fun diceThrow(): Int = (1..6).random()
