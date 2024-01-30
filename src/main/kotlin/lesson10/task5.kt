110f56f1e8e0686c10e3df2dfbba9448bd9f147d
Anatoliy Bokachev <a.bokachev@vk.team>
2024-01-30 13:38:20 +0300

    KS-10-5 created a token generating function and a basket contents printing function

--- src/main/kotlin/lesson10/task5.kt
+++ src/main/kotlin/lesson10/task5.kt
@@ -1,4 +1,32 @@
 package lesson10
 
-class task5 {
+fun main() {
+    println(getBasketContents(authUser("1234", "1234")))
+    println(getBasketContents(authUser("test", "1234")))
+}
+
+fun authUser(login: String, password: String): String? {
+    val digits = 0..9
+    val letters = 'a'..'z'
+    var counter = 32
+    val token = mutableListOf<String>()
+    val userLogin = "test"
+    val userPassword = "1234"
+
+    return if (login == userLogin && password == userPassword) {
+        while (counter > 0) {
+            if (counter % 2 == 0) {
+                token.add(digits.random().toString())
+            } else {
+                token.add(letters.random().toString())
+            }
+            counter--
+        }
+        token.joinToString("")
+    } else null
+}
+
+fun getBasketContents(token: String?): List<String>? {
+    val basket = listOf("Мука", "Хлеб", "Масло", "Вода")
+    return if (token != null) basket else null
 }
