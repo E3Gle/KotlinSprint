09667c30179d74aaf9bc7d40e69586fedfa37077
Anatoliy Bokachev <a.bokachev@vk.team>
2024-01-31 10:51:59 +0300

    KS-11-4 created classes Category, Recipe and Ingredient

--- src/main/kotlin/lesson11/task4.kt
+++ src/main/kotlin/lesson11/task4.kt
@@ -1,4 +1,14 @@
 package lesson11
 
-class task4 {
-}
+class Category(val image: Image, val title: String, val description: String)
+class Recipe(
+    val cover: Image,
+    var isFavorite: Boolean,
+    val title: String,
+    val ingredientsList: List<Ingredient>,
+    var portions: Int
+)
+
+class Ingredient(val title: String, val amount: Int)
+
+class Image(val id: Int, val src: String)
