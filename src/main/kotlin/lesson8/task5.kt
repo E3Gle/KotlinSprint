d45c849ff20ea753a9dc448822844720ad41f73d
Anatoliy Bokachev <a.bokachev@vk.team>
2024-01-31 09:30:11 +0300

    KS-8-5 changed realisation of a function

--- src/main/kotlin/lesson8/task5.kt
+++ src/main/kotlin/lesson8/task5.kt
@@ -3,14 +3,9 @@ package lesson8
 fun main() {
     println("Введите количество ингредиентов в рецепте: ")
     val ingredientsCount = readln().toInt()
-    var counter = 0
-    var ingredients = arrayOf<String>()
 
-    while (counter < ingredientsCount) {
-        println("Введите название ингредиента для добавления в список: ")
-        val userInput = readln()
-        ingredients = ingredients.plus(userInput)
-        counter++
-        if (counter == ingredientsCount) println(ingredients.joinToString(", "))
-    }
+    println("Поочерёдно введите название ингредиентов для добавления в список: ")
+    val ingredients = Array(ingredientsCount) { readln() }
+
+    println(ingredients.joinToString(", "))
 }
