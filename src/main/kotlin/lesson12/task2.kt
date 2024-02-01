ac4212a2b999c2ef2dfb025ddf72e93cc97e6e46
Anatoliy Bokachev <a.bokachev@vk.team>
2024-02-01 10:28:50 +0300

    KS-12-2 modified a weather monitoring class

--- src/main/kotlin/lesson12/task2.kt
+++ src/main/kotlin/lesson12/task2.kt
@@ -1,4 +1,15 @@
 package lesson12
 
-class task2 {
+fun main() {
+
+    val day1 = DaysWeather(13, 15, false)
+
+    day1.printDayWeather()
+}
+
+class DaysWeather(var dayWeather: Int, var nightWeather: Int, var isPrecipitated: Boolean) {
+
+    fun printDayWeather() {
+        println("dayWeather = $dayWeather, nightWeather = $nightWeather, isPrecipitated = $isPrecipitated")
+    }
 }
