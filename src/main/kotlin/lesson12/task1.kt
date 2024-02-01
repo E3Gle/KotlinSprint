76af22347e2d7ec7307c617a992cd67b7c0275db
Anatoliy Bokachev <a.bokachev@vk.team>
2024-02-01 10:24:55 +0300

    KS-12-1 created a weather monitoring class

--- src/main/kotlin/lesson12/task1.kt
+++ src/main/kotlin/lesson12/task1.kt
@@ -1,4 +1,28 @@
 package lesson12
 
-class task1 {
+fun main() {
+
+    val day1 = WeatherOfTheDay()
+    day1.updateWeatherData(12, 11, true)
+    val day2 = WeatherOfTheDay()
+    day2.updateWeatherData(14, 12, true)
+    day1.printDayWeather()
+    day2.printDayWeather()
+}
+
+class WeatherOfTheDay {
+    private var dayWeather: Int = 0
+    private var nightWeather: Int = 0
+    private var isPrecipitated: Boolean = false
+
+    fun updateWeatherData(dayWeather: Int, nightWeather: Int, isPrecipitated: Boolean) {
+        this.dayWeather = dayWeather
+        this.nightWeather = nightWeather
+        this.isPrecipitated = isPrecipitated
+
+    }
+
+    fun printDayWeather() {
+        println("dayWeather = $dayWeather, nightWeather = $nightWeather, isPrecipitated = $isPrecipitated")
+    }
 }
