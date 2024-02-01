90550bf80f9522c262415909e4bb367bf56f9580
Anatoliy Bokachev <a.bokachev@vk.team>
2024-02-01 10:30:40 +0300

    KS-12-2 brought back a method for updating class fields

--- src/main/kotlin/lesson12/task2.kt
+++ src/main/kotlin/lesson12/task2.kt
@@ -7,7 +7,13 @@ fun main() {
     day1.printDayWeather()
 }
 
-class DaysWeather(var dayWeather: Int, var nightWeather: Int, var isPrecipitated: Boolean) {
+class DaysWeather(private var dayWeather: Int,private var nightWeather: Int,private var isPrecipitated: Boolean) {
+
+    fun updateWeatherData(dayWeather: Int, nightWeather: Int, isPrecipitated: Boolean) {
+        this.dayWeather = dayWeather
+        this.nightWeather = nightWeather
+        this.isPrecipitated = isPrecipitated
+    }
 
     fun printDayWeather() {
         println("dayWeather = $dayWeather, nightWeather = $nightWeather, isPrecipitated = $isPrecipitated")
