c799fdd801eced030e9ddb3bf1e611559a5c9f61
Anatoliy Bokachev <a.bokachev@vk.team>
2024-02-02 11:50:36 +0300

    KS-13-4 created a function for adding contacts to contacts list

--- src/main/kotlin/lesson13/task4.kt
+++ src/main/kotlin/lesson13/task4.kt
@@ -1,4 +1,42 @@
 package lesson13
 
-class task4 {
+fun main() {
+
+    val contacts = mutableListOf<ContactsScheme>()
+
+    addContactToListOfContacts(contacts)
+    addContactToListOfContacts(contacts)
+    addContactToListOfContacts(contacts)
+
+    contacts.forEach { it.printContactInfo() }
+
+}
+
+fun addContactToListOfContacts(list: MutableList<ContactsScheme>) {
+    println("Введите имя контакта: ")
+    val userInputName = readln()
+    println("Введите номер телефона контакта: ")
+    val userInputPhoneNumber = readln().toLongOrNull()
+    println("Введите название компании контакта: ")
+    val userInputCompanyName = readln()
+
+    when {
+        userInputPhoneNumber == null -> println("Вы не указали номер телефона, контакт не будет добавлен в список контактов!")
+        userInputCompanyName == "" -> list.add(
+            ContactsScheme(
+                userInputName,
+                userInputPhoneNumber,
+                null
+            )
+        )
+
+        else -> list.add(ContactsScheme(userInputName, userInputPhoneNumber, userInputCompanyName))
+    }
+}
+
+class ContactsScheme(val name: String, val phoneNumber: Long?, val companyName: String?) {
+
+    fun printContactInfo() {
+        println("- Имя: $name\n- Номер: $phoneNumber\n- Компания: ${companyName ?: "<не указано>"}")
+    }
 }
