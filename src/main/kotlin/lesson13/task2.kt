bdb6d22eb5cead754ccd5173043d3451b70edd21
Anatoliy Bokachev <a.bokachev@vk.team>
2024-02-02 10:20:47 +0300

    KS-13-2 created a function for contact info printing

--- src/main/kotlin/lesson13/task2.kt
+++ src/main/kotlin/lesson13/task2.kt
@@ -1,4 +1,14 @@
 package lesson13
 
-class task2 {
+fun main() {
+
+    val user = Contacts("Claire", 5550005555, null)
+    user.printContactInfo()
+
+}
+
+class Contacts(val name: String, val phoneNumber: Long, val companyName: String?) {
+    fun printContactInfo() {
+        println("- Имя: $name\n- Номер: $phoneNumber\n- Компания: ${companyName ?: "<не указано>"}")
+    }
 }
