9e3a676acffbc2c4209d36989f40185471033967
Anatoliy Bokachev <a.bokachev@vk.team>
2024-02-02 12:01:13 +0300

    KS-13-5 created a try-catch block for contact creation flow

--- src/main/kotlin/lesson13/task5.kt
+++ src/main/kotlin/lesson13/task5.kt
@@ -1,4 +1,20 @@
 package lesson13
 
-class task5 {
+fun main() {
+
+    println("Введите номер телефона: ")
+    try {
+        val userInput = readln().toLong()
+        val newContact = AnotherContactsList("Frodo", userInput, "Ring Bearers")
+        newContact.printContactInfo()
+    }catch (e: NumberFormatException){
+        println("Для указания номера телефона используйте только цифры!")
+    }
+
+}
+class AnotherContactsList(val name: String, val phoneNumber: Long?, val companyName: String?) {
+
+    fun printContactInfo() {
+        println("- Имя: $name\n- Номер: $phoneNumber\n- Компания: ${companyName ?: "<не указано>"}")
+    }
 }
