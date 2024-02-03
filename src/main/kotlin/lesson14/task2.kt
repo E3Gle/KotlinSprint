36931a353200a7668d22a65a3d1a16f3162e7179
Anatoliy Bokachev <a.bokachev@vk.team>
2024-02-03 12:00:37 +0300

    KS-14-2 modified classes describing ships types

--- src/main/kotlin/lesson14/task2.kt
+++ src/main/kotlin/lesson14/task2.kt
@@ -1,4 +1,45 @@
 package lesson14
 
-class task2 {
+fun main() {
+
+    val flagship = Flagship1()
+    val freighter = Freighter1()
+    val icebreaker = Icebreaker1()
+
+    flagship.loadCargo()
+    flagship.printInfo()
+
+    freighter.loadCargo()
+    freighter.printInfo()
+
+    icebreaker.loadCargo()
+    freighter.printInfo()
+
+}
+
+open class Flagship1(val speed: Int = 20, val carryingCapacity: Int = 20, val passengersCapacity: Int = 100) {
+    open fun printInfo() {
+        println("Скорость: $speed, грузоподъёмность: $carryingCapacity, количество мест для пассажиров: $passengersCapacity")
+    }
+
+    open fun loadCargo() {
+        println("Выдвигаю горизонтальный трап со шкафута для погрузки...")
+    }
+}
+
+class Freighter1 : Flagship1(speed = 10, carryingCapacity = 100, passengersCapacity = 20) {
+    override fun loadCargo() {
+        println("Активирую погрузочный кран для погрузки...")
+    }
+}
+
+class Icebreaker1(val isIceBreaking: Boolean = true) :
+    Flagship1(speed = 9, carryingCapacity = 10, passengersCapacity = 10) {
+    override fun printInfo() {
+        println("Скорость: $speed, грузоподъёмность: $carryingCapacity, количество мест для пассажиров: $passengersCapacity, может ли ломать лёд: $isIceBreaking")
+    }
+
+    override fun loadCargo() {
+        println("Открываю ворота со стороны кормы для погрузки...")
+    }
 }
