415fb31f9beccfb82bb532461ff76b25e9404a96
Anatoliy Bokachev <a.bokachev@vk.team>
2024-02-03 11:52:28 +0300

    KS-14-1 created classes describing ships types

--- src/main/kotlin/lesson14/task1.kt
+++ src/main/kotlin/lesson14/task1.kt
@@ -1,4 +1,16 @@
 package lesson14
 
-class task1 {
+fun main() {
+
+    val flagship = Flagship()
+    val freighter = Freighter()
+    val icebreaker = Icebreaker()
+
 }
+
+open class Flagship(val speed: Int = 20, val carryingCapacity: Int = 20, val passengersCapacity: Int = 100)
+
+class Freighter : Flagship(speed = 10, carryingCapacity = 100, passengersCapacity = 20)
+
+class Icebreaker(val isIceBreaking: Boolean = true) :
+    Flagship(speed = 9, carryingCapacity = 10, passengersCapacity = 10)
