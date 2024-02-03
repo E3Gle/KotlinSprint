a95bf24523df540193335173d6be850813e6729b
Anatoliy Bokachev <a.bokachev@vk.team>
2024-02-03 12:52:42 +0300

    KS-14-4 created classes describing celestial bodies

--- src/main/kotlin/lesson14/task4.kt
+++ src/main/kotlin/lesson14/task4.kt
@@ -1,4 +1,34 @@
 package lesson14
 
-class task4 {
+fun main() {
+
+    val satellitePhobos = Satellite("Phobos", true, false)
+    val satelliteDeimos = Satellite("Deimos", false, false)
+    val planetMars = Planet("Mars", true, false, listOf(satellitePhobos, satelliteDeimos))
+
+    print(
+        "Planets is: ${planetMars.name}, its satellites are: ${
+            planetMars.listOfSatelllites.joinToString(", ") { it.name }
+        }"
+    )
+
 }
+
+class Planet(
+    override val name: String,
+    override val isLandable: Boolean,
+    override val hasAtmosphere: Boolean,
+    val listOfSatelllites: List<Satellite>
+) : CelestialBody()
+
+class Satellite(
+    override val name: String,
+    override val isLandable: Boolean,
+    override val hasAtmosphere: Boolean
+) : CelestialBody()
+
+open class CelestialBody(
+    open val name: String = "",
+    open val isLandable: Boolean = false,
+    open val hasAtmosphere: Boolean = false
+)
