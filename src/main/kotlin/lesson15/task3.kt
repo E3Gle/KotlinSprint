ae842ded503eafaad5bbdfed4860cac0e011997c
Anatoliy Bokachev <a.bokachev@vk.team>
2024-02-04 10:33:18 +0300

    KS-15-3 created user classes for a forum

--- src/main/kotlin/lesson15/task3.kt
+++ src/main/kotlin/lesson15/task3.kt
@@ -1,4 +1,49 @@
 package lesson15
 
-class task3 {
+fun main() {
+    val simpleUser = SimpleForumUser("Leon", 1)
+    val adminUser = AdminForumUser("Claire", 2)
+
+    simpleUser.sendMessage()
+    simpleUser.readMessage()
+
+    adminUser.readMessage()
+    adminUser.sendMessage()
+    adminUser.deleteMessage()
+    adminUser.deleteUser()
+
+}
+
+class AdminForumUser(username: String, userId: Int) : ForumUser(username, userId) {
+    override fun readMessage() {
+        println("Админ юзер прочитал сообщение")
+    }
+
+    override fun sendMessage() {
+        println("Админ юзер отправил сообщение")
+    }
+
+    fun deleteMessage() {
+        println("Админ юзер удалил сообщение")
+    }
+
+    fun deleteUser() {
+        println("Админ юзер удалил пользователя с форума")
+    }
+}
+
+class SimpleForumUser(username: String, userId: Int) : ForumUser(username, userId) {
+    override fun readMessage() {
+        println("Обычный юзер прочитал сообщение")
+    }
+
+    override fun sendMessage() {
+        println("Обычный юзер отправил сообщение")
+    }
+}
+
+abstract class ForumUser(val username: String, val userId: Int) {
+    abstract fun readMessage()
+    abstract fun sendMessage()
+
 }
