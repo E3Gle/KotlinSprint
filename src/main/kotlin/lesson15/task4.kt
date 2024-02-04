c510acdfa062f00abc4bc43f0e5b7787175f071d
Anatoliy Bokachev <a.bokachev@vk.team>
2024-02-04 10:44:04 +0300

    KS-15-4 created classes for instruments and components

--- src/main/kotlin/lesson15/task4.kt
+++ src/main/kotlin/lesson15/task4.kt
@@ -1,4 +1,21 @@
 package lesson15
 
-class task4 {
+fun main() {
+
+    val guitar = Instrument("Guitar", 12)
+    val guitarStrings = Component("Guitar strings", 20)
+
+    guitar.searchForGoods()
+}
+
+class Instrument(name: String, amountLeft: Int) : WarehouseGoods(name, amountLeft), WarehouseSearch
+
+class Component(name: String, amountLeft: Int) : WarehouseGoods(name, amountLeft)
+
+abstract class WarehouseGoods(val name: String, val amountLeft: Int)
+
+interface WarehouseSearch {
+    fun searchForGoods() {
+        println("Выполняется поиск...")
+    }
 }
