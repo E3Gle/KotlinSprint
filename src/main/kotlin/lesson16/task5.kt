e68f33dc70ae7dc49d2afeaf19d81781ba00dbb3
Anatoliy Bokachev <a.bokachev@vk.team>
2024-02-05 11:06:55 +0300

    KS-16-5 created a Playa class with private and public methods and fields

--- src/main/kotlin/lesson16/task5.kt
+++ src/main/kotlin/lesson16/task5.kt
@@ -1,4 +1,34 @@
 package lesson16
 
-class task5 {
+fun main(){
+
+    val player = Playa("Ada", 10, 5)
+
+    player.takePunch(5)
+    player.getHealing(2)
+    player.takePunch(7)
+    player.getHealing(2)
+
+}
+
+class Playa(val name: String, private var health: Int, var strength: Int) {
+
+    private var isDead = false
+
+    fun takePunch(receivedDamage: Int) {
+        health -= receivedDamage
+        println("Health now is: $health")
+        if (health <= 0) becomeDead()
+    }
+
+    fun getHealing(receivedHealing: Int) {
+        if (!isDead) health += receivedHealing
+        println("Health now is: $health")
+    }
+
+    private fun becomeDead() {
+        isDead = true
+        health = 0
+        strength = 0
+    }
 }
