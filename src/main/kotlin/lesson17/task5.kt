8be54a3ce3984dba732d5478f154593558442b93
Anatoliy Bokachev <a.bokachev@vk.team>
2024-02-11 11:04:27 +0300

    KS-17-5 created a NewestUser class with setters and getter

--- src/main/kotlin/lesson17/task5.kt
+++ src/main/kotlin/lesson17/task5.kt
@@ -1,4 +1,26 @@
 package lesson17
 
-class task5 {
+fun main() {
+
+    val userLeon = NewestUser("Leon", "1234")
+
+    userLeon.login = "New Leon"
+    userLeon.password = "4321"
+    println(userLeon.password)
+}
+
+class NewestUser(login: String, password: String) {
+
+    var login = login
+        set(value) {
+            field = value
+            println("Логин успешно изменён")
+        }
+    var password = password
+        set(value) {
+            println("Вы не можете изменить пароль")
+        }
+        get() {
+            return "*".repeat(field.length)
+        }
 }
