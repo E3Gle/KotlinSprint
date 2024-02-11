621b503056dc105fdf6f792933d97862fbd943fa
Anatoliy Bokachev <a.bokachev@vk.team>
2024-02-11 11:12:18 +0300

    KS-13-2 added default value for a companyName field

--- src/main/kotlin/lesson13/task2.kt
+++ src/main/kotlin/lesson13/task2.kt
@@ -2,12 +2,12 @@ package lesson13
 
 fun main() {
 
-    val user = Contacts("Claire", 5550005555, null)
+    val user = Contacts("Claire", 5550005555)
     user.printContactInfo()
 
 }
 
-class Contacts(val name: String, val phoneNumber: Long, val companyName: String?) {
+class Contacts(val name: String, val phoneNumber: Long, val companyName: String? = null) {
     fun printContactInfo() {
         println("- Имя: $name\n- Номер: $phoneNumber\n- Компания: ${companyName ?: "<не указано>"}")
     }
