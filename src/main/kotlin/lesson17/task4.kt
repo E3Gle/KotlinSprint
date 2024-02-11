dea3b1d927bb315d865f4e5004d771af38769bb0
Anatoliy Bokachev <a.bokachev@vk.team>
2024-02-11 10:21:39 +0300

    KS-17-4 created a Parcel class with a setter

--- src/main/kotlin/lesson17/task4.kt
+++ src/main/kotlin/lesson17/task4.kt
@@ -1,4 +1,21 @@
 package lesson17
 
-class task4 {
+fun main() {
+
+    val parcel = Parcel(12, "Moscow")
+
+    println(parcel.locationChangesCounter)
+    parcel.location = "Tula"
+    println(parcel.locationChangesCounter)
+
+}
+
+class Parcel(val trackingNumber: Int, location: String) {
+    var locationChangesCounter = 0
+
+    var location = location
+        set(value) {
+            field = value
+            locationChangesCounter++
+        }
 }
