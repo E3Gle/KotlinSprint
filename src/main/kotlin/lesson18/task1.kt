61294b376f13f939196c05d0febbae2aa392f26c
Anatoliy Bokachev <a.bokachev@vk.team>
2024-02-20 10:54:39 +0300

    KS-18-1 created a CRM order class

--- src/main/kotlin/lesson18/task1.kt
+++ src/main/kotlin/lesson18/task1.kt
@@ -1,4 +1,20 @@
 package lesson18
 
-class task1 {
+fun main() {
+    val orderCRM = CRMOrder()
+
+    orderCRM.addOrder(1, "Ball")
+    println()
+    orderCRM.addOrder(2, listOf("Soap", "Bread"))
+}
+
+class CRMOrder {
+
+    fun addOrder(orderNumber: Int, goods: String) {
+        println("Order number is $orderNumber, goody is $goods")
+    }
+
+    fun addOrder(orderNumber: Int, goods: List<String>) {
+        println("Order number is $orderNumber, goodies are ${goods.joinToString(", ")}")
+    }
 }
